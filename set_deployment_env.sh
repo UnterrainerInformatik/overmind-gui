3f3795b4c2623bbcc81f4c92f7f1f01b37e6301f
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2021-07-24 14:39:37 +0200

    fix server address

--- set_deployment_env.sh
+++ set_deployment_env.sh
@@ -3,6 +3,6 @@
 ## This file will be used in the docker-compose.yml file automatically because of its name and location.
 ## So this is the place where to transfer the CI-variables to docker-compose.
 echo "VUE_APP_TITLE=Overmind" >> .env
-echo "VUE_APP_PROTOCOL=http" >> .env
-echo "VUE_APP_ADDRESS=10.10.196.4" >> .env
-echo "VUE_APP_PORT=1280" >> .env
+echo "VUE_APP_PROTOCOL=https" >> .env
+echo "VUE_APP_ADDRESS=overmind-server.unterrainer.info" >> .env
+echo "VUE_APP_PORT=443" >> .env
