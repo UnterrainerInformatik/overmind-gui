203709eaa69e17068a7c82cac839b68576f1f76d
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2022-01-02 15:41:38 +0100

    add all plugs and fix some lights

--- src/views/KioskPlugs.vue
+++ src/views/KioskPlugs.vue
@@ -51,6 +51,116 @@ export default {
         title: 'cellar-washingmachine-right',
         appId: 0,
         iconPos: [56, 125]
+      },
+      {
+        title: 'cellar-vestibule',
+        appId: 0,
+        iconPos: [230, 185]
+      },
+      {
+        title: 'cellar-workshop-fridge',
+        appId: 0,
+        iconPos: [270, 109]
+      },
+      {
+        title: '1st-playroom-window',
+        appId: 0,
+        iconPos: [790, 126]
+      },
+      {
+        title: '1st-living-patio-right',
+        appId: 0,
+        iconPos: [582, 282]
+      },
+      {
+        title: '1st-patio-left',
+        appId: 0,
+        iconPos: [573, 335]
+      },
+      {
+        title: '1st-patio-right',
+        appId: 0,
+        iconPos: [688, 335]
+      },
+      {
+        title: '2nd-bath-sink-left-upper',
+        appId: 0,
+        iconPos: [1118, 125]
+      },
+      {
+        title: '2nd-bath-sink-left-lower',
+        appId: 0,
+        iconPos: [1158, 125]
+      },
+      {
+        title: '2nd-bath-sink-right',
+        appId: 0,
+        iconPos: [1118, 75]
+      },
+      {
+        title: '2nd-bath-entrance-right',
+        appId: 0,
+        iconPos: [1158, 185]
+      },
+      {
+        title: '2nd-vestibule',
+        appId: 0,
+        iconPos: [1015, 205]
+      },
+      {
+        title: '2nd-bed-left',
+        appId: 0,
+        iconPos: [1215, 255]
+      },
+      {
+        title: '2nd-bed-right',
+        appId: 0,
+        iconPos: [1215, 330]
+      },
+      {
+        title: '2nd-bedroom-entrance-lower-right',
+        appId: 0,
+        iconPos: [1120, 253]
+      },
+      {
+        title: '2nd-bedroom-entrance-upper-left',
+        appId: 0,
+        iconPos: [1120, 298]
+      },
+      {
+        title: '2nd-wardrobe',
+        appId: 0,
+        iconPos: [1070, 295]
+      },
+      {
+        title: '2nd-alex-entrance',
+        appId: 0,
+        iconPos: [965, 145]
+      },
+      {
+        title: '2nd-alex-bed',
+        appId: 0,
+        iconPos: [920, 186]
+      },
+      {
+        title: '2nd-alex',
+        appId: 0,
+        iconPos: [935, 60]
+      },
+      {
+        title: '2nd-steven-bed',
+        appId: 0,
+        iconPos: [920, 235]
+      },
+      {
+        title: '2nd-steven-entrance',
+        appId: 0,
+        iconPos: [965, 272]
+      },
+      {
+        title: '2nd-steven',
+        appId: 0,
+        iconPos: [965, 320]
       }
     ]
   }),
