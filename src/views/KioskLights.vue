45acde1a250382650862d38a0e26e7aaefbc8e47
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-05-27 14:22:09 +0200

    another update

--- src/views/KioskLights.vue
+++ src/views/KioskLights.vue
@@ -37,28 +37,10 @@ export default {
   data: () => ({
     areas: [
       {
-        title: 'cellar-vestibule',
-        appId: 0,
-        iconPos: [195, 160],
-        coords: [166, 148, 268, 149, 268, 223, 166, 224]
-      },
-      {
-        title: 'cellar-stairs',
-        appId: 0,
-        iconPos: [220, 82],
-        coords: [165, 56, 267, 57, 267, 146, 164, 145]
-      },
-      {
         title: '2nd-bath-mirror',
         appId: 0,
         iconPos: [1120, 90],
         coords: [1120, 159, 1166, 159, 1166, 91, 1120, 91]
-      },
-      {
-        title: '2nd-stairs',
-        appId: 108,
-        iconPos: [1070, 85],
-        coords: [1016, 64, 1117, 64, 1117, 162, 1017, 163]
       },
       {
         title: '2nd-closet',
