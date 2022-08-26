1501c4bc856e8ac768757be5faa2431b25e29230
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2022-08-26 15:41:56 +0200

    add colorPicker
    add debouncer

--- src/components/floorplan/dialogs/FloorplanBulbDialog.vue
+++ src/components/floorplan/dialogs/FloorplanBulbDialog.vue
@@ -1,16 +1,22 @@
 <template>
   <div v-if="app && item">
-    <br>
-    <v-color-picker
-      class="ml-16"
-      v-if="value"
-      :value="value"
-      @update:color="changeColor"
-      dot-size="31"
-      mode="rgba"
-      swatches-max-height="100"
-      v-model="colorModel"
-    ></v-color-picker>
+    <v-row>
+      <v-col v-if="colorModel">
+        <div id="container" class="mb-4" style="width: 300px; height: 50px; position: relative;">
+          <div id="navi" :style="`width: 100%; height: 100%; position: absolute; top: 0; left: 0; background-color: rgba(${colorModel.r}, ${colorModel.g}, ${colorModel.b}, ${colorModel.a})`">&nbsp;</div>
+          <div id="infoi" :style="`width: 100%; height: 100%; position: absolute; top: 0; left: 0; z-index: 10; background-color: rgba(255, 255, 255, ${white / 100})`">&nbsp;</div>
+        </div>
+        <v-color-picker
+          @update:color="setColor"
+          dot-size="31"
+          mode="rgba"
+          v-model="colorModel"
+        ></v-color-picker>
+      </v-col>
+      <v-col>
+        <v-slider v-if="white !== undefined" v-model="white" class="mySlider" :style="cssVars" color="on" :thumb-color="getThumbColor(white)" vertical thumb-label="always" @mouseup="mouseUpWhite" @mousedown="mouseDownWhite"></v-slider>
+      </v-col>
+    </v-row>
     <br>
     <v-btn @click="toggle()" block height="42" :color="
             (app.state.relays[0].state === 'ON' ? 'on ' : 'off ') +
@@ -23,6 +29,8 @@
 </template>
 
 <script lang="js">
+import { Debouncer } from '@/utils/debouncer'
+import { singleton as jsUtils } from '@/utils/jsUtils'
 import { singleton as appliancesService } from '@/utils/webservices/appliancesService'
 
 export default {
@@ -37,39 +45,76 @@ export default {
   },
 
   data: () => ({
+    debouncer: new Debouncer(500),
     pause: false,
     colorModel: null,
-    value: null
+    white: null
   }),
 
   computed: {
+    cssVars () {
+      return {
+        '--my-thumb-color': this.getTextColor(this.white)
+      }
+    }
   },
 
   watch: {
   },
 
   methods: {
-    async changeColor () {
-      this.pause = true
-      await this.setColors()
+    getThumbColor (white) {
+      let v = white / 100
+      if (v < 0.15) {
+        v = 0.15
+      }
+      const result = `rgba(255, 255, 255, ${v})`
+      return result
+    },
+    getTextColor (white) {
+      let v = 0
+      if (white < 50) {
+        v = 255
+      }
+      return `rgba(${v}, ${v}, ${v}, 1)`
+    },
+    async mouseUpWhite () {
+      await this.saveValues()
       this.pause = false
     },
-    getColors (app) {
+    mouseDownWhite () {
+      this.pause = true
+    },
+    getWhite (app) {
+      if (app && app.state && app.state.rgbws && app.state.rgbws[0] && app.state.rgbws[0].white !== undefined) {
+        return app.state.rgbws[0].white * 100
+      }
+      return null
+    },
+    async setColor () {
+      this.pause = true
+      await this.saveValues()
+      this.pause = false
+    },
+    getValues (app) {
+      if (app && app.state && app.state.rgbws && app.state.rgbws[0] && app.state.rgbws[0].white !== undefined) {
+        this.white = app.state.rgbws[0].white * 100
+      }
       if (app && app.state && app.state.rgbws && app.state.rgbws[0]) {
         const v = app.state.rgbws[0]
-        return {
+        this.colorModel = {
           r: v.red * 255,
           g: v.green * 255,
           b: v.blue * 255,
           a: v.gain
         }
       }
-      return null
     },
-    async setColors () {
-      console.log('setColors')
-      const v = this.colorModel.rgba
-      await appliancesService.setColor(this.app.id, 'light', v.r / 255, v.g / 255, v.b / 255, v.a)
+    async saveValues () {
+      const v = this.colorModel
+      this.debouncer.debounce(() => {
+        appliancesService.setColor(this.app.id, 'light', v.r / 255, v.g / 255, v.b / 255, this.white / 100, v.a)
+      })
     },
     async toggle () {
       const actorPath = this.getActorPathOf(this.app, this.item.index)
@@ -99,12 +144,12 @@ export default {
   },
 
   mounted () {
-    this.value = this.getColors(this.app)
+    this.getValues(this.app)
     setInterval(() => {
       if (this.pause) {
         return
       }
-      this.value = this.getColors(this.app)
+      this.getValues(this.app)
     }, 500)
   }
 }
@@ -112,6 +157,10 @@ export default {
 
 <style lang="scss">
 @import '../../index.scss';
+
+.mySlider .v-slider__thumb-label {
+  color: var(--my-thumb-color);
+}
 
 .middle {
   font-size: 15px;
