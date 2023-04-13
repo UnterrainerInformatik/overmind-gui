aab02aa894b7bf62cdc2bfebaec99cce8895a11a
Gerald <gerald@unterrainer.info>
2023-04-13 12:49:32 +0200

    new color for waste-panel

--- src/components/KioskWasteDisposalPanel.vue
+++ src/components/KioskWasteDisposalPanel.vue
@@ -112,7 +112,7 @@ export default {
         now.setDate(now.getDate() - 1)
         const d = []
 
-        d.push(this.findFirstFutureDate(now, this.wasteDisposalJson.wasteBinDates, this.waste, 'waste', 'deep-orange darken-2', this.wasteWarnDaysBefore + 1))
+        d.push(this.findFirstFutureDate(now, this.wasteDisposalJson.wasteBinDates, this.waste, 'waste', 'deep-orange', this.wasteWarnDaysBefore + 1))
         d.push(this.findFirstFutureDate(now, this.wasteDisposalJson.organicWasteBinDates, this.organic, 'organic', 'green darken-3', this.organicWarnDaysBefore + 1))
         d.push(this.findFirstFutureDate(now, this.wasteDisposalJson.plasticWasteBinDates, this.plastic, 'plastic', 'yellow darken-1', this.plasticWarnDaysBefore + 1))
         d.push(this.findFirstFutureDate(now, this.wasteDisposalJson.paperWasteBinDates, this.paper, 'paper', 'red darken-2', this.paperWarnDaysBefore + 1))
